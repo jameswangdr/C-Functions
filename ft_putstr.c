915f652966caec3dc4211b429edd11f6ee3f9d54
James Wang <43688796+jameswangdr@users.noreply.github.com>
2019-01-14 13:50:07 -0800

    Update ft_putstr.c

--- ft_putstr.c
+++ ft_putstr.c
@@ -8,10 +8,10 @@ void	ft_putchar (char c)
 void	ft_putstr (char *str)
 {
 	int index;
-
+	
 	index = 0;
 	while (str[index] != '\0')
-	{ 
+	{
 		ft_putchar (str[index]);
 		index++;
 	}
@@ -25,3 +25,4 @@ int		main ()
 	ft_putchar('\n');
 	return (0);
 }
+
