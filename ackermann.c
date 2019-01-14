121da1e20919cd79cc843c69e3ca6d2c009ea574
James Wang <43688796+jameswangdr@users.noreply.github.com>
2019-01-13 23:01:00 -0800

    ackermann.c
    Shoutout to Wilhelm Ackermann.

---
+++ ackermann.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int ackermann (int m, int n)
+{
+  if (m == 0)
+  return (n + 1);
+  
+  if (m > 0 && n == 0)
+  return (ackermann(m - 1, 1));
+  
+  if (m > 0 && n > 0)
+  return (ackermann(m - 1, ackermann(m, n - 1)));
+}
+
+
+int main ()
+{
+  printf ("%d\n", ackermann ( , ));
+  return (0);
+}
